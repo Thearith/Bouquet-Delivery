0042752bd464c5d1f232ea01146b513c8cca562b
Thearith <sothearith.sreang@gmail.com>
2015-01-20 02:27:44 +0800

    set up user interface

--- BouquetDelivery/app/src/main/java/grababouquet/com/android/bouquetdelivery/MainActivity.java
+++ BouquetDelivery/app/src/main/java/grababouquet/com/android/bouquetdelivery/MainActivity.java
@@ -1,17 +1,93 @@
 package grababouquet.com.android.bouquetdelivery;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+        View.OnClickListener{
+
+    private static final int ID_DIGITS = 9;
+
+    private EditText deliveryIDEditText;
+    private Button startDeliveryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        deliveryIDEditText = (EditText) findViewById(R.id.deliveryIDEditText);
+        startDeliveryButton = (Button) findViewById(R.id.startDeliveryButton);
+        startDeliveryButton.setOnClickListener(this);
+        deliveryIDEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() == ID_DIGITS) {
+                    startDeliveryButton.setEnabled(true);
+                } else {
+                    startDeliveryButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.startDeliveryButton) {
+            String deliveryId = deliveryIDEditText.getText().toString();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = (LayoutInflater)
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.confirm_dialog, null);
+            TextView textView = (TextView) view.findViewById(R.id.deliveryIdTextView);
+            textView.setText(deliveryId);
+
+            builder.setView(view);
+            builder.setCancelable(false);
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener(){
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    submitDeliveryId();
+                }
+            });
+
+            builder.create().show();
+        }
+    }
+
+    private void submitDeliveryId() {
+
     }
 
 
