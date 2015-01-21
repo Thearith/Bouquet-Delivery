20e98fb678f6fa5cba279b1aa7b3b15386b77a72
Thearith <sothearith.sreang@gmail.com>
2015-01-21 18:21:17 +0800

    add check network connection

--- BouquetDelivery/app/src/main/java/grababouquet/com/android/bouquetdelivery/MainActivity.java
+++ BouquetDelivery/app/src/main/java/grababouquet/com/android/bouquetdelivery/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -17,6 +19,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -79,8 +82,12 @@ public class MainActivity extends ActionBarActivity implements
 
     public void onClick(View v) {
         if(v.getId() == R.id.startDeliveryButton) {
-            deliveryId = deliveryIDEditText.getText().toString();
-            requestUserFeed(deliveryId);
+            if (isOnline()) {
+                deliveryId = deliveryIDEditText.getText().toString();
+                requestUserFeed(deliveryId);
+            } else {
+                Toast.makeText(this, "Network isn't available", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -142,6 +149,16 @@ public class MainActivity extends ActionBarActivity implements
         Intent intent = new Intent(MainActivity.this, GeolocationActivity.class);
         intent.putExtra(GeolocationActivity.TAG_ID, deliveryId);
         startActivity(intent);
+    }
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
